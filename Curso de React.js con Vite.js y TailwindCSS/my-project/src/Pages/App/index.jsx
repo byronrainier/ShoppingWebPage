21ee06549ef25a6c2c6ed8813ec9a628e1aac0b0
Byron Henriquez <56271561+byronrainier@users.noreply.github.com>
2024-09-27 15:14:16 -0400

    Subida de archivo de Components

--- Curso de React.js con Vite.js y TailwindCSS/my-project/src/Pages/App/index.jsx
+++ Curso de React.js con Vite.js y TailwindCSS/my-project/src/Pages/App/index.jsx
@@ -5,6 +5,7 @@ import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+import navBar from '../..'
 import './App.css'
 
 const AppRoutes = () => {
